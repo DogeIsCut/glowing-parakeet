7bf450da385f4a2efebc9c9c4265e0fc6283e489
DogeIsCut <DogeIsCut@users.noreply.github.com>
2017-09-30 20:27:25 -0400

    Update Processing.js

--- scripts/Processing.js
+++ scripts/Processing.js
@@ -1,6 +1,7 @@
-draw function = () {
+function draw = () {
     stroke(mouseX, 62, mouseY);
     strokeWeight(20);
     fill(mouseX, mouseY, 0);
     ellipse(mouseX, mouseY, mouseY, mouseX);
 };
+draw
